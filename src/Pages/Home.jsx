3ace4071344232c978a356697959eb41673c8547
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2023-10-30 18:20:36 +0500

    Loader

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,18 +3,21 @@ import { Context as MealsContext } from "../Context/MealsContext";
 import CategoryList from "../components/CategoryList";
 import MealsFiltred from "../components/MealsFilter";
 import { getAllMealsByCategories } from "../Service/getData";
-
+import PuffLoader from "react-spinners/PuffLoader";
 
 const HomePage = () => {
   const { state, dispatch } = React.useContext(MealsContext);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    getAllMealsByCategories().then((res) =>
+    setLoading(true);
+    getAllMealsByCategories().then((res) => {
       dispatch({
         type: "GET_ALL_CATEGORIES",
         payload: res.data.categories,
-      })
-    );
+      });
+      setLoading(false);
+    });
   }, []);
 
   return (
@@ -23,10 +26,22 @@ const HomePage = () => {
         <MealsFiltred />
       </div>
       <div className="my-3">
-        <CategoryList
-          meals={state.meals.slice(0, state.mealsCount)}
-          dispatch={dispatch}
-        />
+        {loading ? (
+          <div className="loader">
+            <PuffLoader
+              color={"red"}
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        ) : (
+          <CategoryList
+            meals={state.meals.slice(0, state.mealsCount)}
+            dispatch={dispatch}
+          />
+        )}
       </div>
     </div>
   );
--- src/components/MealsList/index.jsx
+++ src/components/MealsList/index.jsx
@@ -3,42 +3,58 @@ import { Context as MealsContext } from "../../Context/MealsContext";
 import { getMealsByCategory } from "../../Service/getData";
 import { useParams } from "react-router-dom";
 import MealsItem from "../MealsItem";
+import PuffLoader from "react-spinners/PuffLoader";
 
 const MealsList = () => {
   const { state, dispatch } = React.useContext(MealsContext);
   const { category } = useParams();
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    getMealsByCategory(category).then((res) =>
+    setLoading(true);
+    getMealsByCategory(category).then((res) => {
       dispatch({
         type: "GET_MEALS_CATEGORY",
         payload: res.data.meals,
-      })
-    );
+      });
+      setLoading(false);
+    });
   }, []);
 
   return (
     <div className="container">
-      <ul className="row mt-3">
-        {state.mealsCategoriezed
-          .slice(0, state.mealsCategoriezedCount)
-          .map((meal) => (
-            <MealsItem key={meal.idMeal} {...meal} />
-          ))}
-      </ul>
-      <div className="d-flex justify-content-center">
-        <button
-          className="btn btn-outline-primary my-5 mx-auto"
-          type="button"
-          onClick={() =>
-            dispatch({
-              type: "MEALS_CATEGORIEZED_COUNT",
-            })
-          }
-        >
-          See more
-        </button>
-      </div>
+      {loading ? (
+        <div className="loader">
+          <PuffLoader
+            color={"red"}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <ul className="row mt-3">
+          {state.mealsCategoriezed
+            .slice(0, state.mealsCategoriezedCount)
+            .map((meal) => (
+              <MealsItem key={meal.idMeal} {...meal} />
+            ))}
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-outline-primary my-5 mx-auto"
+              type="button"
+              onClick={() =>
+                dispatch({
+                  type: "MEALS_CATEGORIEZED_COUNT",
+                })
+              }
+            >
+              See more
+            </button>
+          </div>
+        </ul>
+      )}
     </div>
   );
 };
